ed6a394a1955850e8aa70422e68d9f8deb360412
Amayl97 <codeamayl97@gmail.com>
2026-09-23 09:06:41 +0500

    Scraped multiple scholarships

--- multi_scholarship_scrapper.py
+++ multi_scholarship_scrapper.py
@@ -17,7 +17,8 @@ for link in links:
     if url:
         scholarship_links.append(url)
 
-# print(scholarship_links)
+
+print("Total URLs: ", len(scholarship_links))
 
 
 
@@ -25,7 +26,9 @@ field_aliases = {
     "degree_level": ["study level", "degree level", "level of study"],
     "country": ["study location", "host country", "country"],
     "deadline": ["deadline", "application deadline", "last date to apply"],
-    "funding": ["what does the erasmus mundus scholarship covers"],
+    "funding": ["what does the erasmus mundus scholarship covers", "benefits",
+    "scholarship benefits",
+    "benefits of the scholarship"],
     "eligibility": ["eligibility criteria of the erasmus mundus joint masters scholarships", "typical Requirements"],
     "process": ["how to apply for erasmus mundus scholarship"]
 }
@@ -33,7 +36,8 @@ field_aliases = {
 scholarships = []
 
 for url in scholarship_links:
-    scholarship = {
+   try:
+     scholarship = {
     "title": "",
     "country": "",
     "degree_level": [],
@@ -41,27 +45,41 @@ for url in scholarship_links:
     "funding":"",
     "eligibility":[],
     "process":[]
-    }
+     }
     
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
+     response = requests.get(url)
+     soup = BeautifulSoup(response.text, "html.parser")
 
-    title = soup.find("h1").get_text(strip=True)
-    scholarship["title"] = title
+     title = soup.find("h1").get_text(strip=True)
+     scholarship["title"] = title
 
-    headings = soup.find_all("h3")
-    for heading in headings:
-     heading_txt = heading.get_text(strip=True).lower().rstrip(":")
-     for field, aliases in field_aliases.items():
+     headings = soup.find_all("h3")
+     for heading in headings:
+      heading_txt = heading.get_text(strip=True).lower().rstrip(":")
+      for field, aliases in field_aliases.items():
+        if field == "funding" and "benefits" in heading_txt:
+            ul = heading.find_next("ul")
+            if ul:
+               info = []
+               items = ul.find_all("li")
+               if items:
+                for item in items:
+                   info.append(item.get_text(strip=True))
+           
+                   scholarship[field] = info
+               else:
+                  items = []
+                  
+           
          # if aliases matched
-        if heading_txt in aliases:
+        elif heading_txt in aliases:
            ul = heading.find_next("ul")
            if ul:
                info = []
                items = ul.find_all("li")
                if items:
                    for item in items:
-                      info.append(item.get_text(strip=True))
+                     info.append(item.get_text(strip=True))
 
                    scholarship[field] = info
                else:
@@ -69,10 +87,28 @@ for url in scholarship_links:
            else:
                items = []
 
-    scholarships.append(scholarship)
-       
+     scholarships.append(scholarship)
+
+   except Exception as e:
+         print(f"Failed to scrape {url} : {e}")
      
    
 
+# Clean the data
 
-print(scholarships)
+cleaned_scholarships = []
+
+
+
+def clean_text(text):
+   return " ".join(text.split())
+
+for scholarship in scholarships:
+   scholarship["title"] = clean_text(scholarship["title"])
+   scholarship["country"] = clean_text(scholarship["country"])
+   scholarship["deadline"] = clean_text(scholarship["deadline"])
+
+   cleaned_scholarships.append(scholarship)
+
+
+print(cleaned_scholarships)
